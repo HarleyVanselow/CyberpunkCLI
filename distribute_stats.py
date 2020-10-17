80f4d07ac7d7a360376a96b31e69c8c031ad949e
Asic Chen <a19c97@hotmail.com>
2020-10-17 13:44:08 -0400

    fix max_stat logic

--- distribute_stats.py
+++ distribute_stats.py
@@ -1,13 +1,13 @@
 import random
 stats = ['int','ref','attr','luck','cool','tech','ma','bt','emp']
-min = 2
-max = 10
+min_stat = 2
+max_stat = 10
 pool = 40
 result = {}
 random.shuffle(stats)
 for i,stat in enumerate(stats):
-    max = max if (pool-max)/(len(stats)-i) > 2 else min
-    point = random.randrange(min,max+1)
+    max_stat = min(10, pool - (len(stats) - i - 1) * min_stat)
+    point = random.randrange(min_stat, max_stat + 1)
     result[stat] = point
     pool -= point
 print(result)
