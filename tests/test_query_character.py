c1a84a65c821ee39d6041c712abfa4933030a575
harley <harleyvanselow@gmail.com>
2020-10-18 13:15:58 -0400

    Move item and weapon definition to other files

--- tests/test_query_character.py
+++ tests/test_query_character.py
@@ -1,4 +1,5 @@
-from src.router import query_character, get_weapon, Base
+from src.router import query_character, get_weapon_from_character
+from src.displayclasses import Weapon
 
 
 def test_query_character():
@@ -7,21 +8,3 @@ def test_query_character():
     assert (None, '0', 'cowardice') == query_character('harley', 'cowardice')
     assert ('B7', '5', 'int') == query_character('harley', 'int')
 
-
-def test_get_weapon():
-    weapon_dict = {
-        "type": "weapon",
-        "flavor": "C-6",
-        "weapon_type": "HVY",
-        "accuracy": '0',
-        "concealability": "P",
-        "availability": "P",
-        "damage/ammunition": "8D10",
-        "num_shots": '1',
-        "rate_of_fire": '1',
-        "reliability": "VR",
-        "range": "-1",
-        "cost": None
-    }
-    weapon = Base(weapon_dict, None).get_type()
-    assert weapon.__dict__ == get_weapon('harley', 'C-6').__dict__
---
+++ tests/test_query_indexes.py
@@ -0,0 +1,18 @@
+from src.displayclasses import Weapon
+
+
+def test_get_weapon_from_index():
+    weapon = {'flavor': 'Federated Arms Impact',
+              'weapon_type': 'P',
+              'accuracy': '+1',
+              'con': 'J',
+              'avail': 'E',
+              'damage_ammo': '1d6 (.22)',
+              'num_shots': '10',
+              'rof': '2',
+              'rel': 'VR',
+              'range': '50m',
+              'cost': '60'}
+    from_index = Weapon({'flavor':'Impact','type':'weapon'}, None).__dict__
+    for item in weapon.items():
+        assert item in from_index.items()
